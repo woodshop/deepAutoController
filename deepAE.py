f7de6c8f2c286aba47f364ca260219d4c8cf7741
woodshop <asarroff@discovery.bw01.dartmouth.edu>
2013-09-02 14:33:24 -0400

    working class for models created using pylearn2

--- deepAE.py
+++ deepAE.py
@@ -1,14 +1,27 @@
 import numpy as np
 from pylearn2.datasets import dense_design_matrix
 from pylearn2.utils import serial
+from pylearn2.config import yaml_parse
+import sys
+import os
 
 class sevdig8000(dense_design_matrix.DenseDesignMatrix):
-    def __init__(self):
-        rng = 9778
-        path = "${PYLEARN2_DATA_PATH}/7dig8000.npy"
+    """
+    Loads CQFTs for approximately 8000 songs and flattens it such that
+    one CQFT frame is one observation. Normalizes the data first so that the
+    frames of each song are within the range [0,1]
+    """
+    def __init__(self, which_set):
+        rng = [1978, 9, 7]
+        path = "${PYLEARN2_DATA_PATH}/deepAE/7dig8000.npy"
         X = serial.load(path)
         X = np.cast['float32'](X)
         b,m,n = X.shape
+        assert which_set in ['train', 'test']
+        if which_set is 'train':
+            X = X[:2*b/3]
+        else:
+            X = X[2*b/3:]
         X = X.transpose([0,2,1]).reshape((-1, m*n))
         X -= X.min(axis=1, keepdims=True)
         X /= X.max(axis=1, keepdims=True)
@@ -19,3 +32,17 @@ class sevdig8000(dense_design_matrix.DenseDesignMatrix):
                                        rng=rng, preprocessor=None,
                                        fit_preprocessor=False)
         assert not np.any(np.isnan(self.X))
+
+def test_deepAE(name,
+                path='/home/asarroff/projects/deepAutoController/scripts'):
+    script_path = path+'/'+name+'.yaml'
+    f = open(script_path, 'r')
+    yaml = f.read()
+    f.close()
+    yaml = yaml % ({'script': name})
+    train = yaml_parse.load(yaml)
+    train.main_loop()
+
+if __name__ == '__main__':
+    script = sys.argv[1]
+    test_deepAE(script)
